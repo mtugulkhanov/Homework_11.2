e77ee35fe4f27232e538ec49659791a78e4f24b0
tugulkhanov <mtugulkhanov@mail.ru>
2023-05-27 12:12:44 +0800

    add docstring

--- str_func.py
+++ str_func.py
@@ -5,7 +5,7 @@ def to_uppercase(string):
     return string.upper()
 
 """
-функция, которая делает заглавными первые буквы каждого слова в строке
+функция, которая делает заглавными первые буквы каждого слова
 """
 def uppercase_first_letters(string):
     return string.title()
